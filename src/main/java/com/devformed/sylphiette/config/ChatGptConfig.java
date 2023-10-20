c67a9df18357a646a4a461f382cb0ba0ad8a34ad
devformed <rerearitet@gmail.com>
2023-10-20 20:48:56 +0200

    init

--- src/main/java/com/devformed/sylphiette/config/ChatGptConfig.java
+++ src/main/java/com/devformed/sylphiette/config/ChatGptConfig.java
@@ -8,6 +8,8 @@ public record ChatGptConfig(
 		String url,
 		String key,
 		String systemMsg,
+		Integer maxTokens,
+		Double temperature,
 		ChatCompletion.Model model
 ) {
 }
---
+++ src/main/java/com/devformed/sylphiette/i18n/I18n.java
@@ -0,0 +1,67 @@
+package com.devformed.sylphiette.i18n;
+
+import com.google.common.io.Resources;
+import lombok.extern.java.Log;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Optional;
+import java.util.PropertyResourceBundle;
+import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.logging.Level;
+import java.util.stream.Collectors;
+
+@Log
+public class I18n {
+
+	public static final Set<Locale> SUPPORTED_LOCALES = Set.of(toLocale("en"), toLocale("ru"));
+	private static final Map<Locale, ResourceBundle> BUNDLE_BY_LOCALE;
+
+	static {
+		BUNDLE_BY_LOCALE = SUPPORTED_LOCALES.stream()
+				.collect(Collectors.toMap(Function.identity(), I18n::getBundle));
+	}
+
+	public static String translate(String key, Map<String, String> placeHolders, Locale locale) {
+		return Optional.ofNullable(BUNDLE_BY_LOCALE.get(locale))
+				.filter(bundle -> bundle.containsKey(key))
+				.map(bundle -> bundle.getString(key))
+				.map(translation -> applyPlaceholders(translation, placeHolders))
+				.orElse("!" + key + "!");
+	}
+
+	public static String translate(String key, Locale locale) {
+		return translate(key, Collections.emptyMap(), locale);
+	}
+
+	private static String applyPlaceholders(String translation, Map<String, String> placeHolders) {
+		for (Map.Entry<String, String> entry : placeHolders.entrySet()) {
+			translation = translation.replace("${" + entry.getKey() + "}", entry.getValue());
+		}
+		return translation;
+	}
+
+	private static ResourceBundle getBundle(Locale locale) {
+		try {
+			URL resource = Resources.getResource("i18n/translations_" + locale.getLanguage() + ".properties");
+			return new PropertyResourceBundle(new FileReader(new File(resource.toURI())));
+		} catch (IOException e) {
+			log.log(Level.SEVERE, "Failed to load bundle for locale $locale", e);
+			return null;
+		} catch (URISyntaxException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private static Locale toLocale(String languageTag) {
+		return Locale.forLanguageTag(languageTag);
+	}
+}
